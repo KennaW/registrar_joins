5e04b25abc68dc6104f47e3a7638319feef169e3
Cynthia Wilhelm & kenna warsinske <student@epicodus.com>
2015-03-24 11:11:12 -0700

    Create save test and function for Course class

---
+++ src/Course.php
@@ -0,0 +1,79 @@
+<?php
+
+    class Course
+    {
+        private $name;
+        private $course_number;
+        private $id;
+
+        function __construct($name, $course_number, $id = null)
+        {
+            $this->name = $name;
+            $this->course_number = $course_number;
+            $this->id = $id;
+        }
+
+        // getters
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getCourseNumber()
+        {
+            return $this->course_number;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        // setters
+
+        function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        function setCourseNumber($course_number)
+        {
+            $this->course_number = $course_number;
+        }
+
+        function setId($id)
+        {
+            $this->id = $id;
+        }
+
+        // DB functions
+
+        function save()
+        {
+            $statement = $GLOBALS['DB']->query("INSERT INTO courses (course_name, course_number) VALUES ('{$this->getName()}', '{$this->getCourseNumber()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+
+        // static functions
+
+        static function getAll()
+        {
+            $returned_results = $GLOBALS['DB']->query("SELECT * FROM courses;");
+            $courses = [];
+
+            foreach($returned_results as $result) {
+                $name = $result['course_name'];
+                $course_number = $result['course_number'];
+                $id = $result['id'];
+                $new_course = new Course($name, $course_number, $id);
+                array_push($courses, $new_course);
+            }
+            return $courses;
+        }
+    }
+
+
+?>
---
+++ tests/CourseTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+   * @backupGlobals disabled
+   * @backupStaticAttributes disabled
+   */
+
+   require_once "src/Course.php";
+
+   $DB = new PDO('pgsql:host=localhost;dbname=registrar_test');
+
+   class CourseTest extends PHPUnit_Framework_TestCase {
+
+       function test_setName() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number);
+
+           //Act
+           $test_course->setName("World History");
+           $result = $test_course->getName();
+
+           //Assert
+           $this->assertEquals("World History", $result);
+       }
+
+       function test_getName() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number);
+
+           //Act
+           $result = $test_course->getName();
+
+           //Assert
+           $this->assertEquals($name, $result);
+       }
+
+       function test_setCourseNumber() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number);
+
+           //Act
+           $test_course->setCourseNumber("RHIST003");
+           $result = $test_course->getCourseNumber();
+
+           //Assert
+           $this->assertEquals("RHIST003", $result);
+       }
+
+       function test_getCourseNumber() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number);
+
+           //Act
+           $result = $test_course->getCourseNumber();
+
+           //Assert
+           $this->assertEquals("HIST 101", $result);
+       }
+
+       function test_setId() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number, 1);
+
+           //Act
+           $test_course->setId(2);
+           $result = $test_course->getId();
+
+           //Assert
+           $this->assertEquals(2, $result);
+       }
+
+       function test_getId() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST 101";
+           $test_course = new Course($name, $course_number, 1);
+
+           //Act
+           $result = $test_course->getId();
+
+           //Assert
+           $this->assertEquals(1, $result);
+       }
+
+       function test_save() {
+           //Arrange
+           $name = "History of the World Part 1";
+           $course_number = "HIST101";
+           $test_course = new Course($name, $course_number);
+           $test_course->save();
+
+           //Act
+           $result = Course::getAll();
+
+           //Assert
+           $this->assertEquals([$test_course], $result);
+       }
+
+
+}
+
+?>
